0e4c39ffba67d32eeb11f479fe45f3e1f0b2dec4
tushar goel <TG@AMIT-WEBPIXs-MacBook-Air.local>
2018-11-16 18:20:47 +0530

    String CONFIG

--- script.js
+++ script.js
@@ -1,48 +1,80 @@
 arr=['hello','bye','tushar'];
-str='hello';
+cnt=0;
+n=3
+p=5
+str=arr[cnt];
 word1=[];
-for(var element in str)
-{
-word1.push(str[element])
-}
 var qwerty='';
-document.getElementById('test').innerHTML=word1.join('');
+for(var element in str) 
+        {
+            qw=`<t id=${element}>`+str[element]+'</t>'
+            word1.push(qw);
+        } 
+        document.getElementById('test').innerHTML=word1.join('');
 document.getElementById('t1').onkeyup=function(){abc()};
-var count=0;
+if(p==0)
+{
+    console.log('DONE')
+}
+
+var count=0; // Count in Array of Letters
+merac=0
 
 function changeGreen(count)
 {
-word1[count]='<t style="color:green">'+word1[count]+'</t>'
-document.getElementById('test').innerHTML=word1.join('');
+document.getElementById(count).style.color='green';
+merac++
+console.log(merac)
 }
-function changered(count)
+
+function changeRed(count)
 {
-word1[count]='<t style="color:red">'+word1[count]+'</t>'
-document.getElementById('test').innerHTML=word1.join('');
+    document.getElementById(count).style.color='red';
 }
 l2=0;
+
+function check(){
+    if(merac==word1.length)
+    {
+        cnt++;
+        str=arr[cnt];
+        count=0;
+        merac=0
+        l2=0
+        word1=[]
+    for(var element in str) 
+        {
+            qw=`<t id=${element}>`+str[element]+'</t>'
+            word1.push(qw);
+        }   
+        document.getElementById('test').innerHTML=word1.join('');
+        document.getElementById('t1').value=''
+    }
+}
+
 function abc(){
-word=document.getElementById('t1').value;
-l2++;
-qwerty=word[word.length-1];
-l1=word.length;
-if(l1==count+1){    
-if(str[count]===qwerty){
-    console.log(word1[count]);
-        count++;
-        changeGreen(count-1);
-        console.log('green');    
+    word=document.getElementById('t1').value;
+    l2++;
+    qwerty=word[word.length-1];
+    l1=word.length;
+    if(l1==count+1)
+    {    
+        if(str[count]===qwerty)
+        {
+                count++;
+                changeGreen(count-1);
+        }
+        else
+        {
+            count++;
+            changeRed(count-1);
+        }
     }
-    else{
-        count++;
-        changered(count-1);
-        console.log('red');
+    else
+    {
+        document.getElementById(count-1).style.color='black';
+        count--;
     }
-}
-else{
-    count--;
-}
-console.log(l1);
-console.log(count);
+    check();
 }
 
